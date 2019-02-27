a72f2347ee2a8a73dbaaca771d5dafa1a4891a3b
abderahmane44 <46277909+abderahmane44@users.noreply.github.com>
2019-02-26 22:43:32 -1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,171 +1,88 @@
-const prefix ="!";
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-
-client.on('ready', () => {
- console.log("Bot Online"); 
-console.log("log");
-});
-
-
-
-
-var guilds = {};
-client.on('message',async message => {
- var prefix = '!';//البرفكس
-  if(message.content.startsWith(prefix + "buy")) {
  
-if(!message.channel.guild) return message.reply(' ');
- 
- 
-  let submite = message.guild.channels.find(`name`, "112");
- 
-  if(!submite) return message.channel.send("لايوجد روم خاص للي يوصل الشراء اليه :x:");
-  let filter = m => m.author.id === message.author.id;
- 
-    let thisMessage;
- 
-    let thisFalse;
- 
-    message.channel.send('**ما تشتري ؟**').then(msg => {
- 
- 
- 
-    message.channel.awaitMessages(filter, {
- 
-      max: 1,
- 
-      time: 90000,
- 
-      errors: ['time']
- 
-    })
- 
-    .then(collected => {
- 
-      collected.first().delete();
- 
-      thisMessage = collected.first().content;
- 
-      let boi;
- 
-      msg.edit('**الرجاء كتابة اسمك**').then(msg => {
- 
- 
- 
-          message.channel.awaitMessages(filter, {
- 
-            max: 1,
- 
-            time: 90000,
- 
-            errors: ['time']
- 
-          })
- 
-          .then(collected => {
- 
-            collected.first().delete();
- 
-            boi = collected.first().content;
- 
-            let boi2;
- 
-            msg.edit('**الرجاء تحويل المبلغ المطلوب للشخص المطلوب|يوجد لديك 9 ثواني**').then(msg => {
- 
- 
- 
-              message.channel.awaitMessages(filter, {
- 
-                max: 1,
- 
-                time: 90000,
- 
-                errors: ['time']
- 
-              })
- 
-              .then(collected => {
- 
-                collected.first().delete();
- 
-              boi2 = collected.first().content;
- 
-      msg.edit('**هل متاكد من شرائك الرتبة اخي الكريم الرجاء الاجابة بنعم ام لا**');
- 
- message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
- 
-        max: 1,
- 
-        time: 90000,
- 
-        errors: ['time']
- 
-      })
- 
-      .then(collected => {
- 
-        if(collected.first().content === 'لا') {
- 
-          msg.delete();
- 
-          message.delete();
- 
-          thisFalse = false;
- 
-        }
- 
-        if(collected.first().content === 'نعم') {
- 
-          if(thisFalse === false) return;
- 
-          msg.edit('**Done ✅, تم بنجاح شراء الرتبة الرجاء الانتظار حين الادارة ترد عليك**');
- 
-          collected.first().delete();
- 
-          submite.send(`@everyone | @here
-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-**[ ${message.guild.name}:arrow_down: ] Submit**
- 
-[**هل يريد شراء الرتبة او كهدية**]:
-${thisMessage}
- 
-[**اسم الذي يريد شراء الرتبة**]:
-${boi}
- 
-[**هل تم التحويل ام لا**]:
-${boi2}
- 
-[**اسم الشاري بمنشنة**]:
-${message.author}
- 
-[**ايد�� الشاري**]:
-${message.author.id}`);//Fras#2729
- 
-        }
- 
-      }
- 
-  );
- 
-});
- 
-    });
- 
-  }
- 
-    );
- 
-  });
- 
+function clean(text) {
+    if (typeof(text) === "string")
+      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    else
+        return text;
 }
  
-);
+const prefix = "-";
+const token = "NTI2NDAxMzQ2MTYxNzM3NzU5.DwEpFw.narsWvvA881pRPdajKSXseHf48s";
  
-    })}});
-    
-    
-    
-    
-    
-    client.login(process.env.TOKEN); 
+client.on("ready", () => {
+  console.log("FRASGAMER  | Logged in! Server count: ${client.guilds.size}");
+  client.user.setGame(` -help | ${prefix}new`);
+});
+ 
+ 
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+  if (message.content.toLowerCase().startsWith(prefix + `help`)) { //فديتكم كلكم FRAS GAMER
+    const embed = new Discord.RichEmbed()
+    .setTitle(`:mailbox_with_mail: FRASGAMER Help`)
+    .setColor(0xCF40FA)
+    .setDescription(`مرحبا! أنا FRASGAMER ، و Discord commands=بوت لأشياء تذكرة دعم أكثر من رائع وأكثر! وهنا أوامر بلدي:`)
+    .addField(`Tickets`, `[${prefix}new]() > Opens up a new ticket and tags the Support Team\n[${prefix}close]() > Closes a ticket that has been resolved or been opened by accident`)
+    .addField(`Other`, `[${prefix}help]() > Shows you this help menu your reading\n[${prefix}ping]() > Pings the bot to see how long it takes to react\n[${prefix}about]() > Tells you all about FRAS#9999`)
+    message.channel.send({ embed: embed });
+  }
+ 
+  if (message.content.toLowerCase().startsWith(prefix + `ping`)) {
+    message.channel.send(`Hoold on!`).then(m => {
+    m.edit(`:ping_pong: Wew, made it over the ~waves~ ! **Pong!**\nMessage edit time is ` + (m.createdTimestamp - message.createdTimestamp) + `ms, Discord API heartbeat is ` + Math.round(client.ping) + `ms.`);
+    });
+}
+ 
+if (message.content.toLowerCase().startsWith(prefix + `new`)) {
+    const reason = message.content.split(" ").slice(1).join(" ");
+    if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
+    if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
+    message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
+        let role = message.guild.roles.find("name", "Support Team");
+        let role2 = message.guild.roles.find("name", "@everyone");
+        c.overwritePermissions(role, {
+            SEND_MESSAGES: true,
+            READ_MESSAGES: true
+        });
+        c.overwritePermissions(role2, {
+            SEND_MESSAGES: false,
+            READ_MESSAGES: false
+        });
+        c.overwritePermissions(message.author, {
+            SEND_MESSAGES: true,
+            READ_MESSAGES: true
+        });
+        message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
+        const embed = new Discord.RichEmbed()
+        .setColor(0xCF40FA)
+        .addField(`Hey ${message.author.username}!`, `يرجى محاولة شرح سبب فتح هذه التذكرة بأكبر قدر ممكن من التفاصيل. سيكون فريق الدعم ** ** هنا قريباً لمساعدتك.`)
+        .setTimestamp();
+        c.send({ embed: embed });
+    }).catch(console.error);
+}
+if (message.content.toLowerCase().startsWith(prefix + `close`)) {
+    if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`لا يمكنك استخدام أمر الإغلاق خارج قناة التذاكر.`);
+ 
+    message.channel.send(`هل أنت واثق؟ بمجرد التأكيد ، لا يمكنك عكس هذا الإجراء!\للتأكيد ، اكتب \`-تاكيد\`. سوف يتم الغاء امر بعد 10 ثواني ان لم تقم بكتابة تاكيد.`)
+    .then((m) => {
+      message.channel.awaitMessages(response => response.content === '-تاكيد', {
+        max: 1,
+        time: 10000,
+        errors: ['time'],
+      })
+      .then((collected) => {
+          message.channel.delete();
+        })
+        .catch(() => {
+          m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
+              m2.delete();
+          }, 3000);
+        });
+    });
+}
+ 
+});
+ 
